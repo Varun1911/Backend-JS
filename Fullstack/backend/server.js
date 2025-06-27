10e825606f69ee7517d8e96412d7205b1ca67a13
Varun1911 <varunt298@gmail.com>
2025-06-27 18:20:37 +0530

    connect frontend and backend

---
+++ Fullstack/backend/server.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import cors from 'cors';
+
+
+const app = express();
+
+// app.get('/', (req, res) => 
+// {
+//     res.send('Server is ready');
+// });
+
+app.use(cors());
+
+app.get('/api/jokes', (req, res) =>
+{
+    const jokes =
+        [
+            {
+                id: 1,
+                title: 'Joke 1',
+                content: `This is joke 1`
+            },
+            {
+                id: 2,
+                title: 'Joke 2',
+                content: `This is joke 2`
+            },
+            {
+                id: 3,
+                title: 'Joke 3',
+                content: `This is joke 3`
+            },
+            {
+                id: 4,
+                title: 'Joke 4',
+                content: `This is joke 4`
+            },
+            {
+                id: 5,
+                title: 'Joke 5',
+                content: `This is joke 5`
+            }
+        ];
+
+    res.send(jokes);
+});
+
+const port = process.env.port || 4000;
+
+app.listen(port, () => 
+{
+    console.log(`serve at http://localhost:${port}`);
+});
---
+++ Fullstack/frontend/script.js
@@ -0,0 +1,57 @@
+
+async function getJokes()
+{
+    const endpoint = 'http://localhost:4000/api/jokes';
+
+    try
+    {
+        const response = await fetch(endpoint);
+
+        if (!response.ok)
+        {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        return json;
+    }
+
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', async () =>
+{
+    try
+    {
+        const jokes = await getJokes();
+        addJokes(jokes)
+    }
+
+    catch (error)
+    {
+        console.log(error);
+    }
+
+
+    function addJokes(jokes)
+    {
+        let ul = document.querySelector('.jokes ul');
+        console.log(ul)
+
+        jokes.forEach(joke => 
+        {
+            let li = document.createElement('li');
+            let titleNode = document.createTextNode(`${joke.title}`);
+            let contentNode = document.createTextNode(`${joke.content}`);
+            li.appendChild(titleNode);
+            li.innerHTML += '<br>';
+            li.appendChild(contentNode);
+            ul.appendChild(li);
+        }
+        );
+    }
+
+})
